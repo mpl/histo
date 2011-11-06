b803fab1cc245656c71ade36b61cafd463ab89df
mpl <mathieu.lonjaret@gmail.com>
2011-11-06 02:33:48 +0100

    wip. do not use.

--- histo.go
+++ histo.go
@@ -17,9 +17,6 @@ limitations under the License.
 package histo
 
 import (
-	"image"
-	"image/draw"
-	"os"
 	"sort"
 )
 
@@ -28,26 +25,83 @@ type Bar struct {
 	Count int64
 	Min   int64
 	Max   int64
+	
 }
 
-type Histo [](*Bar)
+type sortable []int64 
 
-func (h Histo) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h Histo) Len() int           { return len(h) }
-func (h Histo) Less(i, j int) bool { return h[i].Value < h[j].Value }
+func (s sortable) Swap(i, j int)      {s[i], s[j] = s[j], s[i] }
+func (s sortable) Len() int           { return len(s) }
+func (s sortable) Less(i, j int) bool { return s[i] < s[j] }
 
-func NewHisto(m map[int64]int64, nb int) *Histo {
-	h := make(Histo, len(m))
-	i := 0
-	for k, v := range m {
-		h[i] = &Bar{Value: k, Count: v}
-		i++
+type Histo struct {
+	bar [](*Bar)
+	unsorted sortable
+}
+
+// NewHisto returns an histogram set up with n bins
+func NewHisto() *Histo {
+	return &Histo{nil, nil}
+}
+
+
+func (h Histo) sort() {
+	if h.unsorted != nil {
+		sort.Sort(h.unsorted)
 	}
-	sort.Sort(h)
-	return h.ReBin(nb)
 }
 
-func (h Histo) At(value int64) (hb *Bar) {
+func (h Histo) Add(v int64) {
+	h.unsorted = append(h.unsorted, v)
+}
+
+func (h Histo) bin(num int) {
+	if h.unsorted == nil {
+		return
+	}
+	h.sort()
+	max := h.unsorted[len(h.unsorted) - 1]
+	min := h.unsorted[0]
+	binWidth := 1 + (max - min)/int64(num)
+	np := int64(0)
+	average := int64(0)
+	sup := min + binWidth
+	for _, v := range h.unsorted {
+		if v > sup {
+			average /= np
+			h.bar = append(h.bar, &Bar{average, np, sup, sup + binWidth})
+			sup += binWidth
+			average = v
+			np = 1
+		} else {
+			average += v
+			np++
+		}
+	}
+	h.unsorted = nil
+}
+
+func (h Histo) Bin(num int) {
+	h.bin(num)
+}
+
+func (h Histo) Bars(num int) [](*Bar) {
+	h.bin(num)
+	return h.bar
+}
+
+/*
+func (h Histo) At(num int) (hb *Bar) {
+	if h.bar != nil {
+		if num > len(h.bar - 1) {
+			return nil
+		}
+		return h.bar[num]
+	}
+	return nil 
+}
+
+func (h Histo) Value(value int64) (hb *Bar) {
 	for _, v := range h {
 		if v.Value == value {
 			hb = v
@@ -74,7 +128,7 @@ func (h Histo) MinValue() int64 {
 	return h[0].Value
 }
 
-// TODO(mpl): variable width bins
+
 func (h Histo) ReBin(nb int) *Histo {
 	binWidth := 1 + (h.MaxValue()-h.MinValue())/int64(nb)
 	nh := make(Histo, 0, 1)
@@ -95,45 +149,4 @@ func (h Histo) ReBin(nb int) *Histo {
 	}
 	return &nh
 }
-
-type Params struct {
-	fg draw.Image
-	bg draw.Image
-	op draw.Op
-}
-
-func NewParams(fg draw.Image, bg draw.Image, op draw.Op) (p *Params) {
-	return &Params{fg: fg, bg: bg, op: op}
-}
-
-func (histo Histo) Draw(params *Params) (err os.Error) {
-	if params.bg == nil {
-		return os.NewError("Need a background image to draw on")
-	}
-	bg := params.bg
-	w := bg.Bounds().Dx()
-	h := bg.Bounds().Dy()
-	var fg draw.Image
-	if params.fg == nil {
-		// defaults to plain green bars
-		green := image.RGBAColor{0, 255, 0, 255}
-		fg = image.NewNRGBA(w, h)
-		draw.Draw(fg, fg.Bounds(), &image.ColorImage{green}, image.ZP, draw.Src)
-	} else {
-		fg = params.fg
-	}
-
-	barWidth := w / (2*len(histo) + 1)
-	spacing := barWidth
-	barHeight := 0
-	pos := spacing
-	highestCount := histo.MaxCount()
-
-	for _, hb := range histo {
-		barHeight = int(hb.Count * int64(h) / highestCount)
-		r := image.Rect(pos, h-barHeight, pos+barWidth, h)
-		draw.Draw(bg, r, fg, r.Min, params.op)
-		pos += barWidth + spacing
-	}
-	return err
-}
+*/
